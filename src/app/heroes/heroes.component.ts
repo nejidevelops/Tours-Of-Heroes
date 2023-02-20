deaabaa68da7517bb751bd3468b0d9fcbd3566e3
Newton Ombese <newtonombese1@gmail.com>
2023-02-20 22:28:27 +0300

    :fire:delete: Removing the HEROES import from heroes component ts file

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 import { Hero } from '../hero';
 
-import { HEROES } from '../mock-heroes';
-
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
