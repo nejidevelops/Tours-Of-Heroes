3bc5192a5d45cb5e7e709e53f8af238a4bfa6320
Newton Ombese <newtonombese1@gmail.com>
2023-02-20 22:29:51 +0300

    :recycle:refactor: Replacing the definition of the heroes property with a declaration.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 
 import { Hero } from '../hero';
 
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
